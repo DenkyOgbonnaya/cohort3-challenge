84c7ab3db1f845e28e30b84d3c3d7daedd6c11ea
DenkyOgbonnaya <denkyogb@gmail.com>
2019-10-19 15:14:28 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import UserForm from './components/UserForm';
 
 function App() {
+  const[isVisible, setIsVisble] = useState(true);
+
+  const handleAdd = () => {
+    setIsVisble(!isVisible)
+  }
+  const handleCancel = () => {
+    setIsVisble(!isVisible)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserForm
+        isVisible = {isVisible}
+        handleAdd = {handleAdd}
+        handleCancel = {handleCancel}
+       />
     </div>
   );
 }
